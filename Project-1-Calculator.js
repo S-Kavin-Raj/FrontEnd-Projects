f7cc90b654550b13443c375979b0b192789a6853
S-Kavin-Raj <skavinraj4102006@gmail.com>
2025-07-19 14:51:02 +0530

    new

---
+++ Project-1-Calculator.js
@@ -0,0 +1,13 @@
+var input = document.querySelector("input");
+function addvalue(getvalue) {
+    input.value += getvalue;
+}
+function AC() {
+    input.value = "";
+}
+function Delete() {
+    input.value = input.value.slice(0, input.value.length - 1);
+}
+function evaluate() {
+input.value +=eval(input.value);
+}
